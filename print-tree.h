9993ffebee107cfba726fb946372f2b18a04f420
Gui Hecheng <guihc.fnst@cn.fujitsu.com>
2014-05-16 09:23:37 +0800

    btrfs-progs: add sys_chunk_array and backup roots info to show-super
    Add sys chunk array and backup roots info if the new option '-f'
    if specified.
    This may be useful for debugging sys_chunk related issues.
    
    Signed-off-by: Gui Hecheng <guihc.fnst@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- print-tree.h
+++ print-tree.h
@@ -22,4 +22,5 @@ void btrfs_print_leaf(struct btrfs_root *root, struct extent_buffer *l);
 void btrfs_print_tree(struct btrfs_root *root, struct extent_buffer *t, int follow);
 void btrfs_print_key(struct btrfs_disk_key *disk_key);
 void print_extent_item(struct extent_buffer *eb, int slot, int metadata);
+void print_chunk(struct extent_buffer *eb, struct btrfs_chunk *chunk);
 #endif
